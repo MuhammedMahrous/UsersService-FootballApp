1c2040255eabe60dad0cc2a6b07bb644412fb8c2
MuhammedMahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2018-08-11 03:20:30 +0200

    hello world

--- src/main/java/com/mahrous/footballapp/users/UsersServiceApplication.java
+++ src/main/java/com/mahrous/footballapp/users/UsersServiceApplication.java
@@ -2,11 +2,24 @@ package com.mahrous.footballapp.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+	
+}
+
+@RestController
+class HelloRest{
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
 	}
 }
